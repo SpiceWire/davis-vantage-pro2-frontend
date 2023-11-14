8a9b30388781b00d186cf810637d3c501d68c9ad
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-11-13 22:36:40 -0500

    added dynamic css to one of the forecast icons

--- src/components/ForecastVisual.vue
+++ src/components/ForecastVisual.vue
@@ -10,30 +10,69 @@ const forecastIcon = computed(() => {
   return store.clima.forecastIcon
 })
 
-var iconNameString
+var iconNameString="fa-cloud"
 var iconColorString
 
+const cssProp = computed(() =>{
+  switch (forecastIcon.value) {
+    case 2: return "'--icon-color-string':(red)";  
+      break;
+    case 3: iconColorString="fa-cloud-rain";
+            
+      break;
+    case 6: return "--icon-color-string: conic-gradient(at 53% 50%, yellow 0deg 90deg, blue 90deg 205deg, yellow 205deg 360deg)";
+      break;
+    case 7: iconColorString="fa-cloud-sun-rain";
+            
+      break;
+    case 8: return "'--icon-color-string':(red)";
+            
+      break;
+    case 18:  iconColorString="fa-snowflake";
+              
+      break;
+    case 19:  iconColorString="fa-cloud-meatball";
+              
+      break;
+    case 22:  iconColorString="fa-cloud-meatball";
+             
+      break;
+    case 23:  iconColorString="fa-poo-storm";
+             
+      break;
+    default:  iconColorString="fa-xmarks-lines";
+}
+
+
+})
 function forecastIconSelect() {
   switch (forecastIcon.value) {
-    case 2: iconNameString= "fa-cloud";
+    case 2: iconNameString="fa-cloud";
       break;
-    case 3: iconNameString= "fa-cloud-rain";
+    case 3: iconNameString="fa-cloud-rain";
       break;
-    case 6: iconNameString= "fa-cloud-sun";
+    case 6: iconNameString="fa-cloud-sun";
       break;
-    case 7: iconNameString= "fa-cloud-sun-rain";
+    case 7: iconNameString="fa-cloud-sun-rain";
+            
       break;
-    case 8: iconNameString= "fa-sun";
+    case 8: iconNameString="fa-sun";
+            
       break;
-    case 18: iconNameString= "fa-snowflake";
+    case 18:  iconNameString="fa-snowflake";
+              
       break;
-    case 19: iconNameString= "fa-cloud-meatball";
+    case 19:  iconNameString="fa-cloud-meatball";
+              
       break;
-    case 22: iconNameString= "fa-cloud-meatball";
+    case 22:  iconNameString="fa-cloud-meatball";
+             
       break;
-    case 23: iconNameString= "fa-poo-storm";
+    case 23:  iconNameString="fa-poo-storm";
+             
       break;
-    default: iconNameString= "fa-xmarks-lines";
+    default:  iconNameString="fa-xmarks-lines";
+             
   }
 }
 
@@ -41,7 +80,9 @@ function forecastIconSelect() {
 
 var forecastIconIcon = computed(() => {
   // return "fa-solid " + forecastIconSelect()
-  return "fa-solid" + iconNameString
+  forecastIconSelect()
+  console.log("iconmamestring= " + iconNameString)
+  return "fa-solid " + iconNameString
 })
 </script>
 
@@ -49,9 +90,10 @@ var forecastIconIcon = computed(() => {
 <template>
   <div class="forecastArea">
   <div >
-    <v-icon class="forecast-icon" :icon="forecastIconIcon" />
-    {{ iconNameString }}
+    <v-icon class="forecast-icon" :icon="forecastIconIcon" :style="cssProp"/>
+    
   </div>
+  {{ iconNameString }}
   <div>Forecast</div>
   </div>
 </template>
@@ -71,8 +113,9 @@ var forecastIconIcon = computed(() => {
   align-items: right;
 
   text-align: right;
-  background: conic-gradient(at 53% 50%, yellow 0deg 90deg, blue 90deg 205deg, yellow 205deg 360deg);
-  background: var(forecastIconSelect()[1]);
+  /* background: conic-gradient(at 53% 50%, yellow 0deg 90deg, blue 90deg 205deg, yellow 205deg 360deg); */
+  background: var(--icon-color-string);
+  
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
