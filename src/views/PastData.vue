d901a2cf90395b15d105a371ff3775177fa709e0
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-12-27 01:58:19 -0500

    V-text field appears in PastData

--- src/views/PastData.vue
+++ src/views/PastData.vue
@@ -3,78 +3,42 @@
         <v-container>
             <h2>Past Weather</h2>
             <br>
-            WeatherDate = {{ weatherDate }}
-            <br>
-          
-
             <h3>Click a button to get past data</h3>
             <br>
-            <v-chip v-for="name in btnNames" :value="name.value" @click="useButtonData(name.keyword, name.value)">{{ name.text
+            <v-chip v-for="name in btnNames" :value="name.value" @click="useButtonData(name.keyword, name.value)">{{
+                name.text
             }}</v-chip>
             <br>
             <br>
             <v-row>
-    <v-col
-      cols="12"
-      sm="6"
-      md="4"
-    >
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="weatherDate"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-text-field
-            v-model="weatherDate"
-            label="Click to enter a date"
-            prepend-icon="fa-regular fa-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker
-          v-model="weatherDate"
-          no-title
-          day-format="fullDateWithWeekday"
-		  min="2023-12-13"
-         :max="getUTCDate(new Date(Date.now()))"
-         scrollable
-        >
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="menu = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-            @click="getPastData('day', getDaysOffset(weatherDate))"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
-      </v-menu>
-    </v-col>
-	</v-row>
-
+                <v-col cols="12" sm="6" md="4">
+                    <v-menu 
+                    
+                    :close-on-content-click="false" 
+                    :return-value.sync="weatherDate"
+                    transition="scale-transition" 
+                    offset-y 
+                    min-width="auto"
+                    >
+                        <template v-slot:activator="{ props }" >
+                            <v-text-field 
+                                v-bind:="props"
+                                v-model="weatherDate" 
+                                label="Click to select a date"
+                                prepend-icon="fa-regular fa-calendar" 
+                                readonly 
+                                ></v-text-field>
+                        </template>
+                        
+                    </v-menu>
+                </v-col>
+            </v-row>
             <div>
                 <strong>Weather: {{ weatherDate }} </strong>
             </div>
             <v-row v-for="param, val in dayWeather" :key="val">
                 <v-col cols="6"> {{ val }}</v-col> <v-col cols="4">{{ param }}</v-col>
             </v-row>
-
-
-
         </v-container>
 
     </div>
@@ -86,12 +50,13 @@ import { useDate } from 'vuetify/lib/framework.mjs';
 
 const store = useRecordStore()
 
+var menu1 = ref(true)
 
-var useWeatherDate =useDate();
+var useWeatherDate = useDate();
 var weatherDate = useWeatherDate.format(new Date(), "fullDateWithWeekday")
 
 function formatDate(focusDate) {
-    return useWeatherDate.format(focusDate,"fullDateWithWeekday" )
+    return useWeatherDate.format(focusDate, "fullDateWithWeekday")
 }
 
 var dayWeather = computed(() => {
@@ -106,9 +71,9 @@ const btnNames = ref([
 ])
 
 
-function useButtonData(keyword, offset){
+function useButtonData(keyword, offset) {
     const tokenDate = new Date()
-    tokenDate.setDate(tokenDate.getDate() -offset)
+    tokenDate.setDate(tokenDate.getDate() - offset)
     weatherDate = formatDate(tokenDate)
     console.log("weatherDate3 =" + weatherDate)
     getPastData(keyword, offset)
