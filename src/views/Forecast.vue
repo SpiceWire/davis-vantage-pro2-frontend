d66fab117666adde096cc4331c8106452498ff22
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2024-02-15 17:57:19 -0500

    deleted unused code

--- src/views/Forecast.vue
+++ src/views/Forecast.vue
@@ -4,6 +4,15 @@
     </div>
 </template>
 <script setup>
+import { useForecastStore } from '@/store/forecast';
+import { onUnmounted, onMounted, computed } from 'vue'
+
+const store = useForecastStore();
+
+var forecast=computed(()=>{
+  return store.forecast
+})
+
 
 </script>
 <style >
