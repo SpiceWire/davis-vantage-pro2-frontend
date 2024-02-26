8665e2d5a88a393a6a2cdf1e0f27eb3d9b174d76
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2024-02-26 16:35:26 -0500

    State abbreviation auto-capitalizes

--- src/components/NWSForecast.vue
+++ src/components/NWSForecast.vue
@@ -1,26 +1,32 @@
 <template >
     <div>
         <v-container>
-         <v-row>
-            <v-col>
+
+         <v-row :class="forecastNumber">
+            <v-col cols="3">
                 {{ forecastName }}
             </v-col>
-            <v-col>
-                {{ forecastStarttime }}
-            </v-col>
-            <v-col>
+            <v-col cols="2">
                 {{ forecastTemperature }}&deg;{{ forecastTemperatureUnit }}
             </v-col>
-            <v-col>
-                
+            <v-col cols="1">
+                {{ forecastPrecipChance }}
             </v-col>
+            <v-col cols="auto">
+                {{ forecastDetailedForecast }}
+            </v-col>
+         </v-row>
+         <v-row>
+
          </v-row>
         </v-container>
     </div>
 </template>
 <script setup>
 import { useDate } from 'vuetify'
-import { onUnmounted, onMounted, computed } from 'vue'
+import { onUnmounted, onMounted, computed, ref } from 'vue'
+
+
 
 const aDate = useDate()
 const props = defineProps(['forecastPeriod'])
@@ -34,13 +40,14 @@ var forecastName =computed(()=>
     props?.forecastPeriod?.name
 )
 
-var forecastNumber =computed(()=>
-    props?.forecastPeriod?.number
-)
+var forecastNumber =computed(()=>{
+    var oddEven = props?.forecastPeriod?.number
+    return (oddEven%2==0) ? "bg-blue-lighten-5" : ""
+})
 
 var forecastStarttime =computed(()=>{
     var dateString = props?.forecastPeriod?.startTime
-    return new Date(dateString).toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"}) 
+    return new Date(dateString).toLocaleDateString('en-us', { month:"short", day:"numeric"}) 
     
 })
 
@@ -53,11 +60,21 @@ var forecastEndtime =computed(()=>{
 var forecastTemperature =computed(()=>
     props?.forecastPeriod?.temperature
 )
+
 var forecastTemperatureUnit =computed(()=>
     props?.forecastPeriod?.temperatureUnit
 )
 
-
+var forecastPrecipChance =computed(()=>{
+    var precip = props?.forecastPeriod?.probabilityOfPrecipitation?.value
+    return (precip === null) ? " " :precip+"%" 
+})
+var forecastShortForecast =computed(()=>
+    props?.forecastPeriod?.shortForecast
+)
+var forecastDetailedForecast =computed(()=>
+    props?.forecastPeriod?.detailedForecast
+)
 // var forecastItem = {
 //     number: Number,
 //     name: String,
--- src/views/ForecastVue.vue
+++ src/views/ForecastVue.vue
@@ -1,6 +1,62 @@
 <template>
     <div>
-      NWSForecast
+      <v-row>
+             <v-col cols="7">
+              <v-text-field 
+                label="Enter an address for local forecast: " 
+                append-inner-icon="fas fa-magnifying-glass"
+                clearable
+                :rules="[zipRules]"
+                @click:append-inner="findZip"
+                >
+
+              </v-text-field>
+              
+              <v-text-field 
+              id="addressField"
+              v-model="streetAddress"
+              label="Street Address "
+              clearable
+              :rules="[nonBlank]" 
+              >
+              </v-text-field>
+              <v-text-field 
+              id="cityField"
+              
+              label="City: "
+              clearable
+              :rules="[nonBlank]" 
+              >
+              </v-text-field>
+              <v-text-field 
+              id="stateField"
+              v-model="stateAbbrev"
+              label="State: "
+              :rules="[nonBlank, usState]"
+              clearable
+              @input="stateAbbrev = stateAbbrev.toUpperCase()"
+              
+              >
+              </v-text-field>
+              <v-text-field 
+                id="zipField"
+                label="ZIP code: " 
+                clearable
+                :rules="[zipRules]"
+                @click:append-inner="findZip"
+                >
+              </v-text-field>
+
+             </v-col>   
+            <v-row>
+              <v-btn type="submit">Submit</v-btn>
+           
+            </v-row>
+            
+
+                
+            </v-row>
+          
       <NWSForecast v-for="(period, index) in newPropertiesUpdated?.periods" :key="index" :forecastPeriod="period"></NWSForecast>
        
         <br>
@@ -16,14 +72,76 @@
         <br>
         {{ forecast }}
     </div>
+
+ 
 </template>
 <script setup>
 import { useForecastStore } from '@/store/forecast';
-import { onUnmounted, onMounted, computed } from 'vue'
+import { onUnmounted, onMounted, computed, ref } from 'vue'
 import  NWSForecast from '../components/NWSForecast.vue'
 
-const store = useForecastStore();
 
+
+function uppercase(el)  {
+    {
+        const sourceValue = document.getElementById("stateField").value
+        const newValue = sourceValue.toUpperCase()
+
+        if (sourceValue !== newValue) {
+          document.getElementById("stateField").value = newValue
+            var el = el.getElementById("stateField")
+            el.dispatchEvent(new Event("stateField", { bubbles: true }))
+        }
+    }
+}
+
+var capState = computed(() =>{
+  var stateValue = document.getElementById("stateField")
+  if (stateValue?.value){
+     return stateValue?.value?.toUppercase()
+  }
+    
+})
+
+const store = useForecastStore();
+var latLon=ref({"lat":Number, "lon":Number})
+var stateAbbrev = ref()
+var streetAddress = ref()
+function zipRules(value) {
+   return (/(^\d{5}$)|(^\d{5}-\d{4}$)/).test(value) || 'Valid ZIP required'
+}
+
+function nonBlank(value){
+  if (value) return true
+  return 'Field must not be blank'
+}
+
+function usState(value){
+  const usStates = ["AL", "AK", "AZ", "AR", "AS", "CA", "CO", "CT", "DE", "DC",
+   "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+   "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NH", "NM", "NY", "NC", 
+   "ND", "MP", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "TT", 
+   "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY"]
+
+   return usStates.indexOf(value) > -1 || "Valid State name needed"
+}
+
+function capitalizeState(value){
+  console.log("captalize triggered")
+  // var stateValue = document.getElementById("stateField").value 
+  // console.log("stateValue is: ", stateValue)
+  // stateValue = "hello"
+  var stateValue = document.getElementById("stateField")
+  console.log("statevalue is: ", stateValue.value)
+  console.log("capitalize is: ", stateValue.value.toUpperCase())
+  if (stateValue?.value){
+     stateValue.value = stateValue?.value?.toUpperCase()
+  }
+}
+function findZip(value){
+  //http://dev.virtualearth.net/REST/v1/Locations?countryRegion={countryRegion}&adminDistrict={adminDistrict}&locality={locality}&postalCode={postalCode}&addressLine={addressLine}&userLocation={userLocation}&userIp={userIp}&usermapView={usermapView}&includeNeighborhood={includeNeighborhood}&maxResults={maxResults}&key={BingMapsKey}
+  //http://dev.virtualearth.net/REST/v1/Locations/US&postalCode={value}&maxResults=2&key={BingMapsKey}  
+}
 onMounted(()=>{
   store.fetchForecast()
   
@@ -47,6 +165,11 @@ var newPropertiesUpdated =computed(()=>{
 
 
 </script>
-<style >
-    
+<style scoped>
+    .v-field_input .state-field {
+      text-transform: uppercase;
+    }
+    input#stateField {
+      text-transform: uppercase;
+    }
 </style>
