2fd6d57d6cd5e9c4a155f598a70d53d03a7d51c3
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2024-04-25 19:57:13 -0400

    Improved display of Daily Record

--- src/views/RecordsData.vue
+++ src/views/RecordsData.vue
@@ -1,27 +1,43 @@
 <template >
     <div>
         <v-container>
-            <h2>Daily Weather</h2>
+            <h2>Daily Record</h2>
             <br>
-            <h3>Click a button to get a day's data</h3>
+            <h3>Displays weather summary of a date </h3>
             <br>
-            <v-chip v-for="name in btnNames" :value="name.value" @click="useButtonData(name.keyword, name.value)"> {{
+            <v-chip v-for="name in btnNames" :value="name.value" @click="useButtonData(name.keyword, name.value)">{{
                 name.text
             }}</v-chip>
             <br>
             <br>
 
             <v-row xs12 sm6 md4>
-                <DatePicker
-                    label="Start Date"
-                    v-model="startDate"
-                    color="primary"
-                ></DatePicker>
+                        <v-menu 
+                            v-model="isMenuOpen" 
+                            :close-on-content-click="false">
+                            <template v-slot:activator="{ props }">
+                                <v-text-field 
+                                    label="Click to select a date" 
+                                    :model-value="formattedDate" 
+                                    readonly
+                                    v-bind="props" 
+                                    variant="solo" 
+                                    hide-details>
+                                </v-text-field>
+                            </template>
+                            <v-date-picker 
+                                v-model="selectedDate" 
+                                hide-actions="false" 
+                                title="Click to select a date"
+                                :color="color">
+                                <template v-slot:header></template>
+                            </v-date-picker>
+                        </v-menu>
             </v-row>
-               <br>
-               <br> 
+            <br>
+            <br>
             <div>
-                <strong>Weather: {{ weatherDate }} </strong>
+                <strong>Weather: {{ buttonDate }} </strong>
             </div>
             <v-row v-for="param, val in dayWeather" :key="val">
                 <v-col cols="6"> {{ val }}</v-col> <v-col cols="4">{{ param }}</v-col>
@@ -34,32 +50,32 @@
 
 import { useRecordStore } from '@/store/record';
 import { ref, watch, computed } from 'vue';
-import { DatePicker } from '../components/DatePicker.vue'
 
 const store = useRecordStore()
 
-var weatherDate 
 const { label, color, modelValue } = defineProps([
-  "label",
-  "color",
-  "modelValue",
+    "label",
+    "color",
+    "modelValue",
 ]);
-const emit = defineEmits("update:modelValue");
 
-const selectedDate = ref(modelValue);
+const isMenuOpen = ref(false);
+var selectedDate = ref(modelValue);
+var buttonDate
 
 const formattedDate = computed(() => {
-  return selectedDate.value ? selectedDate.value.toLocaleDateString("en") : "";
+    return selectedDate.value ? selectedDate.value.toLocaleDateString("en") : "";
 });
 
-watch(modelValue, (newDate) => {
-  selectedDate.value = newDate;
-});
+function useButtonData(keyword, offset) {
+    getPastData(keyword, offset)
+}
 
-watch(selectedDate, (newDate) => {
-  emit("update:modelValue", newDate);
+watch(selectedDate, () => {
+    isMenuOpen.value = false;
+    const daysOffset = getDaysOffset(selectedDate.value);
+    getPastData("day", daysOffset)
 });
-
 
 var dayWeather = computed(() => {
     return store.getRecord
@@ -72,14 +88,6 @@ const btnNames = ref([
     { text: "Three days ago", keyword: "day", value: 3 },
 ])
 
-
-function useButtonData(keyword, offset) {
-    const tokenDate = new Date()
-    tokenDate.setDate(tokenDate.getDate() - offset)
-    weatherDate = formattedDate(tokenDate)
-    getPastData(keyword, offset)
-}
-
 function getDaysOffset(selectedDate) {
     const MILLS_PER_DAY = 1000 * 60 * 60 * 24;
     const today = getUTCDate(new Date(Date.now()));
@@ -91,9 +99,15 @@ function getUTCDate(validDate) {
     return Date.UTC(validDate.getFullYear(), validDate.getMonth(), validDate.getDate())
 }
 
+function subtractDays(offset) {
+    return new Date(new Date().setDate(new Date().getDate()-offset)).toLocaleDateString("en",
+   {weekday:"short", year:"numeric", month:"short", day:"numeric"})
+}
 
 function getPastData(keyword, offset) {
+    buttonDate=subtractDays(offset)
     store.fetchRecord(keyword, offset)
 }
+
 </script>
 <style ></style>
